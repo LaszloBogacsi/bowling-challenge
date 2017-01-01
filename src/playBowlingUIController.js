092a0ea6b267f5aaac76b27d0ce5c7c063032168
LaszloBogacsi <bogacsi.laszlo@gmail.com>
2017-01-01 20:00:50 +0000

    jquery added back

--- src/playBowlingUIController.js
+++ src/playBowlingUIController.js
@@ -2,45 +2,45 @@ $(document).ready(function(){
   var game = new Game();
 
 
-$("#roll").click(function() {
-  game.newRoll();
-  console.log(game)
-  updateFrameScore();
+  $("#roll").click(function() {
+    game.newRoll();
+    console.log(game)
+    updateFrameScore();
 
-})
+  })
 
-function updateFrameScore() {
+  function updateFrameScore() {
 
-  var frameCount = game.currentFrameNumber;
+    var frameCount = game.currentFrameNumber;
 
-  var roll1 = game.gameFrames[frameCount - 1].rolls[0];
-  var roll2 = game.gameFrames[frameCount - 1].rolls[1];
-  var prevPrevScore = game.gameScore[frameCount - 3];
-  var prevScore = game.gameScore[frameCount - 2];
-  var currScore = game.gameScore[frameCount - 1];
-  var total = game.gameScoreTotal;
+    var roll1 = game.gameFrames[frameCount - 1].rolls[0];
+    var roll2 = game.gameFrames[frameCount - 1].rolls[1];
+    var prevPrevScore = game.gameScore[frameCount - 3];
+    var prevScore = game.gameScore[frameCount - 2];
+    var currScore = game.gameScore[frameCount - 1];
+    var total = game.gameScoreTotal;
 
-  if(frameCount > 10 && roll1 != 10) {
-    $("#roll").prop('disabled', true);
-    $("#roll").text('Game Over');
-    return;
-  } else if (frameCount > 10 && roll1 === 10) {
+    if(frameCount > 10 && roll1 != 10) {
+      $("#roll").prop('disabled', true);
+      $("#roll").text('Game Over');
+      return;
+    } else if (frameCount > 10 && roll1 === 10) {
 
+    }
+
+    if (roll1 === 10) {
+      roll1 = ''
+      roll2 = 'X'
+    } else if (roll1 + roll2 === 10) {
+      roll2 = '/'
+    };
+
+    $(`#roll${(frameCount*2)-2}`).text(roll1);
+    $(`#roll${(frameCount*2)-1}`).text(roll2);
+    $(`#sum${(frameCount-1)}`).text(currScore);
+    $(`#sum${(frameCount-2)}`).text(prevScore);
+    $(`#sum${(frameCount-3)}`).text(prevPrevScore);
+    $("#total").text(total);
   }
-
-if (roll1 === 10) {
-  roll1 = ''
-  roll2 = 'X'
-} else if (roll1 + roll2 === 10) {
-  roll2 = '/'
-};
-
-  $(`#roll${(frameCount*2)-2}`).text(roll1);
-  $(`#roll${(frameCount*2)-1}`).text(roll2);
-  $(`#sum${(frameCount-1)}`).text(currScore);
-  $(`#sum${(frameCount-2)}`).text(prevScore);
-  $(`#sum${(frameCount-3)}`).text(prevPrevScore);
-  $("#total").text(total);
-}
 
 });
--- src/playBowlingUIView.js
+++ src/playBowlingUIView.js
@@ -2,95 +2,82 @@ $(document).ready(function(){
 
   generateTable();
 
+  function generateTable(){
+    generateTableHeader();
+    generateTableBodyData();
+    generateTableBodySum();
+  }
 
-function generateTable(){
-  generateTableHeader();
-  generateTableBodyData();
-  generateTableBodySum();
-}
-
-function generateTableHeader(){
-  var row = document.getElementById('frameNumbers');
-  var cell;
-  var text;
-  var attr;
-  for(var i = 1; i < 10; i++){
-    cell = document.createElement("th");
-    text = document.createTextNode(i);
+  function generateTableHeader(){
+    var row = document.getElementById('frameNumbers');
+    var cell;
+    var text;
+    var attr;
+    for(var i = 1; i < 10; i++){
+      cell = document.createElement("th");
+      text = document.createTextNode(i);
+      cell.appendChild(text);
+      attr = document.createAttribute("colspan")
+      attr.value = 2;
+      cell.setAttributeNode(attr);
+      row.appendChild(cell);
+    }
+    cell =  document.createElement("th");
+    text =  document.createTextNode("10");
     cell.appendChild(text);
     attr = document.createAttribute("colspan")
+    attr.value = 3;
+    cell.setAttributeNode(attr);
+    row.appendChild(cell);
+
+    cell =  document.createElement("th");
+    text =  document.createTextNode("Total");
+    cell.appendChild(text);
+    attr = document.createAttribute("colspan")
+    attr.value = 1;
+    cell.setAttributeNode(attr);
+    row.appendChild(cell);
+  }
+
+  function generateTableBodyData() {
+    var row = document.getElementById('rollScore');
+    for(var i = 0; i < 21; i++){
+      var cell = document.createElement("td");
+      var attr = document.createAttribute("id");
+      attr.value = `roll${i}`;
+      cell.setAttributeNode(attr);
+      row.appendChild(cell);
+    }
+    cell =  document.createElement("td");
+    attr = document.createAttribute("rowspan")
     attr.value = 2;
     cell.setAttributeNode(attr);
+    var attrId = document.createAttribute("id");
+    attrId.value = "total";
+    cell.setAttributeNode(attrId);
     row.appendChild(cell);
   }
-  cell =  document.createElement("th");
-  text =  document.createTextNode("10");
-  cell.appendChild(text);
-  attr = document.createAttribute("colspan")
-  attr.value = 3;
-  cell.setAttributeNode(attr);
-  row.appendChild(cell);
 
-  cell =  document.createElement("th");
-  text =  document.createTextNode("Total");
-  cell.appendChild(text);
-  attr = document.createAttribute("colspan")
-  attr.value = 1;
-  cell.setAttributeNode(attr);
-  row.appendChild(cell);
-}
+  function generateTableBodySum(){
+    var row = document.getElementById('sum');
+    for(var i = 0; i < 9; i++){
+      var cell = document.createElement("td");
+      var idAttr = document.createAttribute("id");
+      var colspAttr = document.createAttribute("colspan");
+      colspAttr.value = 2;
+      cell.setAttributeNode(colspAttr);
+      idAttr.value = `sum${i}`;
+      cell.setAttributeNode(idAttr);
+      row.appendChild(cell);
 
-function generateTableBodyData() {
-  var row = document.getElementById('rollScore');
-  for(var i = 0; i < 21; i++){
-    var cell = document.createElement("td");
-    var attr = document.createAttribute("id");
-    attr.value = `roll${i}`;
+    }
+    cell =  document.createElement("td");
+    attr = document.createAttribute("colspan")
+    attr.value = 3;
     cell.setAttributeNode(attr);
+    var attrId = document.createAttribute("id");
+    attrId.value = "sum9";
+    cell.setAttributeNode(attrId);
     row.appendChild(cell);
   }
-  cell =  document.createElement("td");
-  attr = document.createAttribute("rowspan")
-  attr.value = 2;
-  cell.setAttributeNode(attr);
-  var attrId = document.createAttribute("id");
-  attrId.value = "total";
-  cell.setAttributeNode(attrId);
-  row.appendChild(cell);
-}
-
-function generateTableBodySum(){
-  var row = document.getElementById('sum');
-  for(var i = 0; i < 9; i++){
-    var cell = document.createElement("td");
-    var idAttr = document.createAttribute("id");
-    var colspAttr = document.createAttribute("colspan");
-    colspAttr.value = 2;
-    cell.setAttributeNode(colspAttr);
-    idAttr.value = `sum${i}`;
-    cell.setAttributeNode(idAttr);
-    row.appendChild(cell);
-
-  }
-  cell =  document.createElement("td");
-  attr = document.createAttribute("colspan")
-  attr.value = 3;
-  cell.setAttributeNode(attr);
-  var attrId = document.createAttribute("id");
-  attrId.value = "sum9";
-  cell.setAttributeNode(attrId);
-  row.appendChild(cell);
-}
-
-
-
-
-
-
-
-
-
-
-
-
 });
